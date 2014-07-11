ec90fd8732ce70c7b406458d4407c833a713c777
Ailend <andrewsw@mail.ru>
2014-07-11 16:11:48 +0400

    Add all files.

---
+++ app/build/source/buildConfig/debug/se/jayway/opengl/tutorial/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package se.jayway.opengl.tutorial;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "se.jayway.opengl.tutorial";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+}
