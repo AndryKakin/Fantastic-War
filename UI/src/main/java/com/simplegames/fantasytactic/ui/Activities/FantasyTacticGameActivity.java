c7d346a0f30e1303e9a99de67ae23e7f99d0ed34
Ailend <andrewsw@mail.ru>
2014-07-16 18:12:51 +0400

    Refactoring project.

---
+++ UI/src/main/java/com/simplegames/fantasytactic/ui/Activities/FantasyTacticGameActivity.java
@@ -0,0 +1,24 @@
+package com.simplegames.fantasytactic.ui.Activities;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.simplegames.fantasytactic.ui.OpenGl.OpenGLRenderer;
+
+
+public class FantasyTacticGameActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	this.requestWindowFeature(Window.FEATURE_NO_TITLE); 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+ 		GLSurfaceView view = new GLSurfaceView(this);
+   		view.setRenderer(new OpenGLRenderer());
+   		setContentView(view);
+    }
+}
---
+++ UI/src/main/java/com/simplegames/fantasytactic/ui/OpenGl/DrawObjects/CityUIModel.java
@@ -0,0 +1,41 @@
+package com.simplegames.fantasytactic.ui.OpenGl.DrawObjects;
+
+/**
+ * Created by andrey.kakin on 11.07.14.
+ */
+public class CityUIModel extends Mesh {
+    public CityUIModel() {
+        float width = 1;
+        float height = 1;
+        float depth = 1;
+        width  /= 2;
+        height /= 2;
+        depth  /= 2;
+
+        float vertices[] = { -width, -height, -depth, // 0
+                width, -height, -depth, // 1
+                width,  height, -depth, // 2
+                -width,  height, -depth, // 3
+                -width, -height,  depth, // 4
+                width, -height,  depth, // 5
+                width,  height,  depth, // 6
+                -width,  height,  depth, // 7
+        };
+
+        short indices[] = { 0, 4, 5,
+                0, 5, 1,
+                1, 5, 6,
+                1, 6, 2,
+                2, 6, 7,
+                2, 7, 3,
+                3, 7, 4,
+                3, 4, 0,
+                4, 7, 6,
+                4, 6, 5,
+                3, 0, 1,
+                3, 1, 2, };
+
+        setIndices(indices);
+        setVertices(vertices);
+    }
+}
