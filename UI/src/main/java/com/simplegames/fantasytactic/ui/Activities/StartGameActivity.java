01dcd5f396c5da8e494da2cabc494605144e93b1
Ailend <andrewsw@mail.ru>
2014-07-18 20:36:14 +0400

    Add lictener for onclick.

--- UI/src/main/java/com/simplegames/fantasytactic/ui/Activities/StartGameActivity.java
+++ UI/src/main/java/com/simplegames/fantasytactic/ui/Activities/StartGameActivity.java
@@ -13,6 +13,7 @@ public class StartGameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
+        get
     }
 
 
